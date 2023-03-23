2bb74069249d3d582100c30b33b7143cdbc27623
Jonas Rudbeck Johansen <jrj@thise.dk>
2023-03-23 14:39:09 +0100

    <DebugType>embedded</DebugType> added

--- test_project/xUnitTestTestProject/UnitTest1.cs
+++ test_project/xUnitTestTestProject/UnitTest1.cs
@@ -8,5 +8,13 @@ namespace xUnitTestTestProject
             string test = "hej";
             Assert.Contains("world", test);
         }
+
+        [Fact] public void Test2()
+        {
+            int expected = 10;
+            int actual = 5 + 5;
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
